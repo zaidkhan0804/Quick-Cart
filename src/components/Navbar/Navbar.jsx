1a4d9710df31ded45da1595320665dd74148a8a5
navedkhan777 <naved2508310@gmail.com>
2024-09-09 19:56:34 +0530

    add new features for navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,19 +2,16 @@ import React, { useState } from 'react';
 import './Navbar.css';
 
 const Navbar = () => {
-
-  const [menu,setMenu] = useState("shop");
-
   return (
       <div className="navbar">
         <div className="nav-logo">   
        <p>Shopper</p> 
       </div>
      <ul className="nav-menu">
-      <li onClick={()=>{setMenu("Shop")}}>Shop{menu==="Shop"?<hr/>:<></>}</li>
-      <li onClick={()=>{setMenu("Men")}}>Men{menu==="Men"?<hr/>:<></>}</li>
-      <li onClick={()=>{setMenu("Women")}}>Women{menu==="Women"?<hr/>:<></>}</li>
-      <li onClick={()=>{setMenu("kids")}}>Kids{menu==="kids"?<hr/>:<></>}</li>
+      <li>Shop</li>
+      <li>Men</li>
+      <li>Women</li>
+      <li>Kids</li>
      </ul>
      <div className="nav-logo-cart">
       <button>LogIn</button>
